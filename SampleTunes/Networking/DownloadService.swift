48d5c88dbbc7f2c9c06d03d7d8bbfe8f8b951f12
Leslie <leslie.appl@gmail.com>
2021-01-12 21:42:57 +0800

    Finished DownloadService

---
+++ SampleTunes/Networking/DownloadService.swift
@@ -0,0 +1,35 @@
+//
+//  DownloadService.swift
+//  SampleTunes
+//
+//  Created by leslie on 1/12/21.
+//
+
+import Foundation
+
+/// Downloads song snippets, and stores in local file.
+/// Allows cancel, pause, resume download.
+
+class DownloadService {
+    
+    // MARK: - Variables And Properties
+    var downloadsSession: URLSession!
+    
+    // MARK: - Internal Methods
+    func cancelDownload(_ track: Track) {
+        
+    }
+    
+    func pauseDownload(_ track: Track) {
+        
+    }
+
+    func resumeDownload(_ track: Track) {
+        
+    }
+
+    func startDownload(_ track: Track) {
+        
+    }
+
+}
